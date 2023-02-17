0f1c9440778d1877bee66cf4eed0100875d26b43
Man of Red <0362877@schoolsnet.act.edu.au>
2023-02-17 12:59:09 +1100

    Bullet Limit, Countdown & Player Borders

--- Bullet/Bullet.gd
+++ Bullet/Bullet.gd
@@ -4,6 +4,7 @@ var speed = 500
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	GlobalVariables.bulletInstanceCount += 1
 	set_physics_process(true)
 
 func _physics_process(delta):
