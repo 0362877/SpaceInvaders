055a01e090e2c96c6fb6288d1d11fe84f32f7280
ARg <0362877@schoolsnet.act.edu.au>
2023-03-10 12:50:56 +1100

    Enemy Movement and Bullet Work

--- Bullet-Enemy/Bullet-Enemy.gd
+++ Bullet-Enemy/Bullet-Enemy.gd
@@ -1,16 +1,19 @@
 extends KinematicBody2D
 
+var speed = 200
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	GlobalVariables.enemyBulletInstanceCount += 1
+	set_physics_process(true)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _physics_process(delta):
+	var collidedObject = move_and_collide(Vector2(0, + speed*delta*0.4))
+	if (collidedObject): 
+		#print ("Enemy collide:", collidedObject.collider. name)
+		if "Enemy" in collidedObject.collider.name:
+			pass 
+			#
+		else:
+			queue_free()
+			GlobalVariables.enemyBulletInstanceCount -= 1
+			print ("Enemy Bullets: ", GlobalVariables.enemyBulletInstanceCount)
--- Enemy/Enemy.gd
+++ Enemy/Enemy.gd
@@ -1,6 +1,6 @@
 extends KinematicBody2D
 
-var speed = -200
+var speed = 0
 
 
 func _ready():
@@ -8,4 +8,10 @@ func _ready():
 
 func _physics_process(delta):
 	global_position.x += speed * delta
-
+	var rng = RandomNumberGenerator.new()
+	rng.randomize()
+	var my_random_number = rng.randf_range(2.0, 30.0)
+	yield(get_tree().create_timer(my_random_number), "timeout")
+	if GlobalVariables.enemyBulletInstanceCount < 5:
+		var bulletInstance = bullet.instance()
+#need help
