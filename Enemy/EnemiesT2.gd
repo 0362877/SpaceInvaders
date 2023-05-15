1e77d39ca4ef94080b7ceec2cd854efe6c6a68e2
ARg <0362877@schoolsnet.act.edu.au>
2023-05-15 14:42:13 +1000

    Enemy Type Update

---
+++ Enemy/EnemiesT2.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+
+var speed = 300
+
+
+func _ready():
+	set_physics_process(true)
+
+func _physics_process(delta):
+	global_position.x += speed * delta
+
--- health.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-var health = 0
-export(int) var max_health = 3
-
-func _ready():
-health = max_health
-
-func take_damage(amount)
